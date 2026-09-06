5235965e1ba8f275179c106c66dec74557784f1c
nznofficial <emvazquez.dev@gmail.com>
2026-09-06 11:29:38 -0700

    Chapter 2 - Constants and Formatting

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"unicode/utf8"
 )
 
 func main() {
+	// Chapter 2 - Constants and Formatting
 	var username string
 	username = "chomp_bot"
 
@@ -23,10 +25,23 @@ func main() {
 
 	const basicPlan = "basic"
 
+	const secondsInHour = 3600
+
+	const name = "Saul Goodman"
+	const openRate = 30.54
+	msg := fmt.Sprintf("Hi %s, your open rate is %.1f percent \n", name, openRate)
+
+	const bear = "🐻"
+	fmt.Printf("constant 'name' rune length: %d\n", utf8.RuneCountInString(bear))
+	fmt.Println(bear)
+
 	fmt.Println("username:", username)
 	fmt.Println("isAdmin:", isAdmin)
 	fmt.Println("permissions:", permissions)
 	fmt.Println("costPerSMS:", costPerSMS)
 	fmt.Println(messageStart, age, messageEnd)
 	fmt.Println("Plan:", basicPlan)
+	fmt.Println("secondsInHour:", secondsInHour)
+	fmt.Print(msg)
+
 }
