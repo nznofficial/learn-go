b2ce1c643d4b232aadbe42828c2f6a8bf943a85b
nznofficial <emvazquez.dev@gmail.com>
2026-09-08 22:23:13 -0700

    Chapter 6 - Interfaces

--- main.go
+++ main.go
@@ -72,6 +72,57 @@ func newUser(name string, membershipType string) User {
 	return User{Name: name, Membership: membership}
 }
 
+type notification interface {
+	importance() int
+}
+
+type directMessage struct {
+	senderUsername string
+	messageContent string
+	priorityLevel  int
+	isUrgent       bool
+}
+
+type groupMessage struct {
+	groupName      string
+	messageContent string
+	priorityLevel  int
+}
+
+type systemAlert struct {
+	alertCode      string
+	messageContent string
+}
+
+func (d directMessage) importance() int {
+	if d.isUrgent {
+		return 50
+	}
+	return d.priorityLevel
+}
+
+func (g groupMessage) importance() int {
+	return g.priorityLevel
+}
+
+func (s systemAlert) importance() int {
+	return 100
+}
+
+func processNotification(n notification) (string, int) {
+	//
+	switch v := n.(type) {
+	case directMessage:
+		return v.senderUsername, v.importance()
+	case groupMessage:
+		return v.groupName, v.importance()
+	case systemAlert:
+		return v.alertCode, v.importance()
+	default:
+		return "", 0
+	}
+}
+
 func main() {
 	// Chapter 2 - Constants and Formatting
 	var username string
@@ -256,4 +307,7 @@ outer:
 
 	fmt.Println(newUser1.SendMessage("Hot Wheels", len("Hot Wheels")))
 	fmt.Println(newUser2.SendMessage("Dinosaurs", len("Dinosaurs")))
+
+	// Chapter 6 - Interfaces
+
 }
